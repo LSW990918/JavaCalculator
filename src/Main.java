eaa0d8ae2b281da058214b64645f4cf119c55e1a
leesywon99 <leesywon99@gmail.com>
2024-07-09 15:28:19 +0900

    레벨1 진행중 반복문을 이용한 계산기

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,47 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = sc.nextInt();  // 첫 번째 숫자를 입력받고 변수에 저장합니다.
+        int firstNumber = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = sc.nextInt();  // 두 번째 숫자를 입력받고 변수에 저장합니다.
+        int result = firstNumber;
+        boolean continueCalculation = true;
 
-        System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
-        char operator = sc.next().charAt(0);  // 사칙연산 기호를 입력받고 변수에 저장합니다.
+        while (continueCalculation) {
+            System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+            char operator = sc.next().charAt(0);
 
-        int result = 0;
+            System.out.print("다음 숫자를 입력하세요: ");
+            int nextNumber = sc.nextInt();
 
-        if (operator == '+') {
-            result = firstNumber + secondNumber;
-        } else if (operator == '-') {
-            result = firstNumber - secondNumber;
-        } else if (operator == '*') {
-            result = firstNumber * secondNumber;
-        } else if (operator == '/') {
-            if (secondNumber != 0) {
-                result = firstNumber / secondNumber;
+            if (operator == '+') {
+                result += nextNumber;
+            } else if (operator == '-') {
+                result -= nextNumber;
+            } else if (operator == '*') {
+                result *= nextNumber;
+            } else if (operator == '/') {
+                if (nextNumber != 0) {
+                    result /= nextNumber;
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    continue;
+                }
             } else {
-                System.out.println("0으로 나눌 수 없습니다.");
-                sc.close();
-                return;
+                System.out.println("잘못된 연산자입니다.");
+                continue;
             }
-        } else {
-            System.out.println("잘못된 연산자입니다.");
-            sc.close();
-            return;
+
+            System.out.println("현재 결과: " + result);
+
+            System.out.print("계산을 계속하시겠습니까? (yes/no): ");
+            String userResponse = sc.next();
+
+            if (userResponse.equalsIgnoreCase("no")) {
+                continueCalculation = false;
+            }
         }
 
-        // 결과를 출력합니다.
-        System.out.println("결과: " + result);
-
-        sc.close();  // Scanner를 닫아줍니다.
+        System.out.println("최종 결과: " + result);
+        sc.close();
     }
 }
